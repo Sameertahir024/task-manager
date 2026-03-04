17509c66ff86c0e00f49f4cfbba1762c432b6820
sameer tahir <sameertahir024@gmail.com>
2026-03-04 14:52:14 +0500

    Added Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/tasks-page', [TaskController::class, 'indexPage']);
-// Route::post('/tasks-store', [TaskController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified' ])->name('dashboard');
+// Admin Dashboard
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
